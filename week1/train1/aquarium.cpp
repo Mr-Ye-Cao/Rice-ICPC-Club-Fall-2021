627eed90284a4974cfedf466287d04a8623110fc
Mr-Ye-Cao <caoye_2020@yeah.net>
2021-09-14 15:49:01 -0500

    updated&commented the solution for aquarium(from Charlie Liu)

--- week1/train1/aquarium.cpp
+++ week1/train1/aquarium.cpp
@@ -1,83 +1,103 @@
 /***
- * Sorry I have not finished programming this problem.
- * Please check again this git later 
-**/
+ *  Solution Credit to Charlie Liu
+ *  from Rice ICPC club
+ *  comments added by Peter
+ * **/
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-bool comparePairs(const pair<int,int>& lhs, const pair<int,int>& rhs){
-  return lhs.second < rhs.second;
+int n,d,l;
+vector<pair<double,double>> L;
+vector<pair<double,double>> R;
+
+double find_vol(double h){
+    // since we divided the points into two sections
+    double Larea=0,Rarea=0;
+    // calculate the trapezoid of left sections
+    for(int i=0;i<L.size();i++){
+        if(h<L[i+1].second){
+            double dh=h-L[i].second;
+            double r=dh/(L[i+1].second-L[i].second);
+            Larea+=0.5*dh*((2-r)*L[i].first+r*L[i+1].first);
+            break;
+        }else{
+            Larea+=0.5*(L[i].first+L[i+1].first)*(L[i+1].second-L[i].second);
+        }
+    }
+    // calculate the trapezoid of right sections
+    for(int i=0;i<R.size();i++){
+        if(h<R[i+1].second){
+            double dh=h-R[i].second;
+            double r=dh/(R[i+1].second-R[i].second);
+            Rarea+=0.5*dh*((2-r)*R[i].first+r*R[i+1].first);
+            break;
+        }else{
+            Rarea+=0.5*(R[i].first+R[i+1].first)*(R[i+1].second-R[i].second);
+        }
+    }
+    // the area is the difference of the trapezoid
+    return (Rarea-Larea)*d;
 }
 
-void solve(int N,int D,int L,vector<pair<int,int> >& coords,int H){
-	vector<pair<int,int> > basvert(4),sec1,sec2;
-		
-	bool isFirZero=true,isFirH=true;
-	for(int i=0;i<N;i++)
-		if(coords[i].first==0){
-			if(isFirZero){
-				basvert[0]=coords[i];
-				isFirZero=false;
-			}else
-				basvert[1]=coords[i];
-		}else if(coords[i].second==H){
-			if(isFirH){
-				basvert[2]=coords[i];
-				isFirH=false;
-			}else
-				basvert[3]=coords[i];
-		}
-			
-	double x1=((double)basvert[0].first+(double)basvert[1].first)/2;
-	double y1=(double)0;
-	double x2=((double)basvert[2].first+(double)basvert[3].first)/2;
-	double y2=(double)H;
+int main()
+{   
+    // use a list for constant time insertion and deletion at front and back
+    list<pair<int,int>> vtx;
+    cin>>n>>d>>l;
+    for(int i=0;i<n;i++){
+        int x,y;
+        cin>>x>>y;
+        // read input points into list
+        vtx.push_back(make_pair(x,y));
+    }
+    while(true){
+        // since the input points are given in counterclockwise order
+        // this loop terminates when front and back of list
+        // are points (x1,0) and (x2,0)
+        if(vtx.front().second==0&&vtx.back().second==0){
+            break;
+        }else{
+            vtx.push_back(vtx.front());
+            vtx.pop_front();
+        }
+    }
 
-	double slope,intrcpt,seperline;
-	if(x1!=x2){
-		slope=(y1-y2)/(x1-x2);
-		intrcpt=y1-slope*x1;
-		for(int i=0;i<N;i++)
-			if(slope*coords[i].first+intrcpt<coords[i].second)
-				sec2.push_back(coords[i]);
-			else
-				sec1.push_back(coords[i]);
-	}else{
-		seperline=x1;
-		for(int i=0;i<N;i++)
-			if(coords[i].first>seperline)
-				sec2.push_back(coords[i]);
-			else
-				sec1.push_back(coords[i]);
-	}
+    for(int i=0;i<n;i++){
+        pair<int,int> temp=vtx.front();
+        vtx.pop_front();
+        // keep pushing points into the Right section
+        R.push_back(temp);
+        // the point of break between section1 and section2 happens
+        // when (x3,y1) (x4,y1)
+        if(vtx.front().second==temp.second)
+            break;
+    }
+    // the rest of the points belong to the Left section
+    while(!vtx.empty()){
+        L.push_back(vtx.back());
+        vtx.pop_back();
+    }
+    
+    // next we shift all of the points rightwards 1001 units
+    // this way we are able to calculate the trapezoid area
+    for(int i=0;i<L.size();i++)
+        L[i].first+=1001;
+    for(int i=0;i<R.size();i++)
+        R[i].first+=1001;
 
-	std::sort(sec1.begin(),sec1.end(),comparePairs);
-	std::sort(sec2.begin(),sec2.end(),comparePairs);
-
-	int a=0,b=H;
-	while(a<=b){
-		int mid=(a+b)/2;
-		vector<pair<int,int> >::iterator it1=lower_bound(sec1.begin(),sec1.end(),mid);
-		vector<pair<int,int> >::iterator it2=lower_bound(sec2.begin(),sec2.end(),mid);
-		computeVolume();
-	}
-}
-
-
-int main(){
-	int N,D,L,H=-1;
-	vector<pair<int,int> > coords(N);
-
-	cin>>N>>D>>L;
-	for(int i=0;i<N;i++){
-		int x,y;
-		cin>>x>>y;
-		H=max(H,y);
-		coords[i] = make_pair(x,y);
-	}
-
-	solve(N,D,L,coords,H);
-
-	return 0;
+    // start a binary search with active region (0,height of the aquarium tank)
+    double lo=0;
+    double hi=R.back().second;
+    // given the constraints at most 50 updates (very possibly less) are needed
+    // to find the exact point of height
+    for(int i=0;i<50;i++){
+        double mid=(lo+hi)/2;
+        if(find_vol(mid)>l*1000) hi=mid;
+        else lo=mid;
+    }
+    // print out the height with 2 decimal places
+    printf("%.2f", (lo+hi)/2);
+    return 0;
 }
